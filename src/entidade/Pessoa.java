a5fc628d9d4e13c713f30feb5b6da1a5d9d42ec3
pretto <pretto@prettolap>
2019-05-23 15:09:33 -0300

    Criada classe de testes: TudoTest.java que chama SuiteTest

---
+++ src/entidade/Pessoa.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+/**
+ *
+ * @author pretto
+ */
+public class Pessoa {
+
+    private String nome;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
